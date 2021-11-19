5c915d65774a1984c2c43054866f987be58521c5
Matvey <tem12qaz@yandex.ru>
2021-11-20 02:22:21 +0300

    ee

--- console_tools.py
+++ console_tools.py
@@ -5,7 +5,9 @@ from sys import platform
 def clear():
     if platform == "linux" or platform == "linux2":
         os.system('clear')
+
     elif platform == "darwin":
         os.system('clear')
+
     elif platform == "win32":
         os.system('cls')
--- test.py
+++ test.py
@@ -35,9 +35,9 @@ def str_to_list(string: str, cols, rows):
     return str_list
 
 
-def animation(cols, rows, string=None, animation_color='', string_color='', width=25, char='░'):
+def animation(cols, rows, string=None, animation_color='', string_color='', width=40, char='░', fill=' '):
     if string:
-        string = format_str(string, cols, rows, ' ')
+        string = format_str(string, cols, rows, fill)
         string = str_to_list(string, cols, rows)
 
     to_fill = [(cols, 0)]
@@ -49,6 +49,8 @@ def animation(cols, rows, string=None, animation_color='', string_color='', widt
         y += 1
         to_fill.append((x, y))
 
+    half_width = width//2
+    writed = []
     for i in range(cols + rows + width):
         for x, y in to_fill:
             x = x - max_value + i
@@ -56,20 +58,41 @@ def animation(cols, rows, string=None, animation_color='', string_color='', widt
                 continue
             elif x < 0:
                 continue
-            elif x > cols:
-                if x <= cols + width:
+
+            for x_ in range(x-half_width if x >= half_width else 0, x+half_width if x < cols-half_width else cols):
+                dist = half_width - (x_ - x if x_ > x else x - x_)
+                ver = dist/half_width*100 + random.randint(0, 100)
+
+                if (x_, y) not in writed:
+                    if ver >= 100 and x - x_ != half_width:
+                        print_on_x_y(x_, y, char, animation_color)
+                    else:
+                        print_on_x_y(x_, y, fill, animation_color)
+
+            shift = width//4
+
+            if x > cols:
+                if x <= cols + shift:
                     try:
-                        print_on_x_y(x - width, y, ' ' if not string else string[y][x - width], string_color)
+                        print_on_x_y(x - shift, y, fill if not string else string[y][x - shift], string_color)
+                        if string[y][x - shift] != fill:
+                            writed.append((x - shift, y))
                     except:
-                        print_on_x_y(x - width, y, ' ')
+                        print_on_x_y(x - shift, y, fill)
                 continue
 
-            print_on_x_y(x, y, char, animation_color)
-            if x >= width:
+
+            # print_on_x_y(rand_x-1, y, fill, animation_color)
+            # print_on_x_y(x, y, char, animation_color)
+            if x >= shift:
                 try:
-                    print_on_x_y(x - width, y, ' ' if not string else string[y][x - width], string_color)
+                    print_on_x_y(x - shift, y, fill if not string else string[y][x - shift], string_color)
+                    if string[y][x - shift] != fill:
+                        writed.append((x - shift, y))
                 except:
-                    print_on_x_y(x - width, y, ' ')
+                    print_on_x_y(x - shift, y, fill)
+                writed.append((x, y))
+
         if i % 15 == 0:
             time.sleep(0.000001)
 
@@ -107,7 +130,7 @@ def main():
         color = random.choice(colors)
         columns, rows = shutil.get_terminal_size((80, 20))
         time.sleep(1)
-        animation(columns, rows, test_str, getattr(Output, color), getattr(Output, color), width=25)
+        animation(columns, rows, test_str, getattr(Output, color), getattr(Output, color), width=40, char='█')
         # print(getattr(Output, color) + format_str(test_str, columns, rows) + '\033[0m')
         # time.sleep(3333)
 
